75d961fb65c05534898d41a754239af3f0e0cfc3
Poly Titova <poly_titova@mail.ru>
2020-08-31 17:34:42 +0300

    Сделали базовую структуру

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import { Layout } from "antd";
 
 import "./App.css";
 
+const { Header, Footer, Content } = Layout;
+
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <Layout className="App">
+        <Header className="App-header">
+          <h1>Quick Todo</h1>
+        </Header>
+        <Content className="App-content">Content</Content>
+        <Footer className="App-footer">&copy; My Company</Footer>
+      </Layout>
+    );
   }
 }
 
